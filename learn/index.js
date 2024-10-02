eaaeab30230d1ed6124136f1e9954fa8cda96ddc
belhadj-yahya <bitiljusgamer@gmail.com>
2024-10-02 12:25:48 +0100

    Update index.js

--- learn/index.js
+++ learn/index.js
@@ -1,17 +1,4 @@
-let moroccoP = document.getElementById("morocco");
-let likeMorocco = document.getElementById("morocco-like")
 
-let italyP = document.getElementById("italy");
-let likeItaly = document.getElementById("italy-like");
-
-let norwayP = document.getElementById("norway");
-let likeNorway = document.getElementById("norway-like");
-
-let russiaP = document.getElementById("russia");
-let likeRussia = document.getElementById("russia-like");
-
-let turkeyP = document.getElementById("Turkey");
-let likeTurkey = document.getElementById("Turkey-like");
 
 let mainContainer = document.querySelector('.main');
 
